73ceba4015a83d37dc12f957e3a95820cc4b39f0
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-05-21 11:00:47 +0200

    styles connections page

--- client/src/components/connections.js
+++ client/src/components/connections.js
@@ -49,77 +49,85 @@ export default function Connections() {
     };
 
     return (
-        <div className="lg:w-2/5 md:w-2/3 flex flex-col justify-center items-center">
-            {requests.length === 0 || requests.length > 1 ? (
-                <p className="text-gray-700">
-                    {requests.length} connection requests
-                </p>
-            ) : (
-                <p className="text-gray-700">
-                    {requests.length} connection request
-                </p>
-            )}
-
-            <div
-                className="w-full flex-grow overflow-y-auto mt-8"
-                style={{ height: "40vh" }}
-            >
-                {requests.map((user, index) => {
-                    return (
-                        <div
-                            key={index}
-                            className="group flex items-center justify-between w-full h-28 mt-4 bg-purple-100 rounded-lg border-2 border-purple-200 duration-200 hover:bg-white hover:shadow-lg hover:border-transparent"
-                        >
-                            {userInfo(user)}
-                            <div className="mr-8 py-1 flex flex-col justify-evenly h-full md:w-1/3 lg:w-1/4">
-                                <button
-                                    onClick={() =>
-                                        dispatch(acceptConnection(user.id))
-                                    }
-                                    className="bg-purple-200 font-bold rounded-full w-full p-2 duration-200 hover:bg-purple-300 hover:text-gray-700"
-                                >
-                                    Accept
-                                </button>
+        <div className="w-full">
+            <div className="lg:w-1/2 pl-12 p-4 flex flex-col items-center">
+                {connections.length === 0 || connections.length > 1 ? (
+                    <p className="text-gray-700 text-lg font-bold">
+                        {connections.length} connections
+                    </p>
+                ) : (
+                    <p className="text-gray-700 text-lg font-bold">
+                        {connections.length} connection
+                    </p>
+                )}
+                <div className="w-full">
+                    {connections.map((user, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className="group flex items-center justify-between w-full h-28 mt-4 bg-purple-100 rounded-lg border-2 border-purple-200 duration-200 hover:bg-white hover:shadow-lg hover:border-transparent"
+                            >
+                                {userInfo(user)}
                                 <button
                                     onClick={() =>
                                         dispatch(disconnect(user.id))
                                     }
-                                    className="p-2 text-gray-500 font-bold w-full rounded-full duration-200 hover:bg-purple-50 hover:text-gray-700"
+                                    className="bg-purple-200 font-bold rounded-full md:w-1/3 lg:w-1/4 mr-8 p-2 duration-200 hover:bg-purple-300 hover:text-gray-700"
                                 >
-                                    Decline
+                                    Disconnect
                                 </button>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
-            {connections.length === 0 || connections.length > 1 ? (
-                <p className="text-gray-700">
-                    {connections.length} connections
-                </p>
-            ) : (
-                <p className="text-gray-700">{connections.length} connection</p>
-            )}
-            <div
-                className="w-full flex-grow overflow-y-auto mt-8"
-                style={{ height: "60vh" }}
-            >
-                {connections.map((user, index) => {
-                    return (
-                        <div
-                            key={index}
-                            className="group flex items-center justify-between w-full h-28 mt-4 bg-purple-100 rounded-lg border-2 border-purple-200 duration-200 hover:bg-white hover:shadow-lg hover:border-transparent"
-                        >
-                            {userInfo(user)}
-                            <button
-                                onClick={() => dispatch(disconnect(user.id))}
-                                className="bg-purple-200 font-bold rounded-full md:w-1/3 lg:w-1/4 mr-8 p-2 duration-200 hover:bg-purple-300 hover:text-gray-700"
-                            >
-                                Disconnect
-                            </button>
-                        </div>
-                    );
-                })}
+            <div className="lg:w-1/3 fixed top-16 right-12">
+                <div
+                    className="p-4 bg-white rounded-lg shadow-lg overflow-y-auto"
+                    style={{ maxHeight: "calc(100vh - 8rem)" }}
+                >
+                    {requests.length === 0 || requests.length > 1 ? (
+                        <p className="text-gray-700 text-center text-lg font-bold">
+                            {requests.length} connection requests
+                        </p>
+                    ) : (
+                        <p className="text-gray-700 text-center text-lg font-bold">
+                            {requests.length} connection request
+                        </p>
+                    )}
+                    <div className="w-full overflow-y-auto">
+                        {requests.map((user, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className="group flex items-center justify-between w-full h-28 mt-4 bg-purple-100 rounded-lg border-2 border-purple-200 duration-200 hover:bg-purple-50 hover:border-purple-100"
+                                >
+                                    {userInfo(user)}
+                                    <div className="mr-8 py-1 flex flex-col justify-evenly h-full md:w-1/3 lg:w-1/4">
+                                        <button
+                                            onClick={() =>
+                                                dispatch(
+                                                    acceptConnection(user.id)
+                                                )
+                                            }
+                                            className="bg-purple-200 font-bold rounded-full w-full p-2 duration-200 hover:bg-purple-300 hover:text-gray-700"
+                                        >
+                                            Accept
+                                        </button>
+                                        <button
+                                            onClick={() =>
+                                                dispatch(disconnect(user.id))
+                                            }
+                                            className="p-2 text-gray-500 font-bold w-full rounded-full duration-200 hover:bg-purple-100 hover:text-gray-700"
+                                        >
+                                            Decline
+                                        </button>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     );
