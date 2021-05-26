0cdb7136e7f05895699f143a497ed455b14585b8
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-05-26 15:32:03 +0200

    adds description to welcome page

--- client/src/welcome.js
+++ client/src/welcome.js
@@ -6,13 +6,18 @@ import PasswordReset from "./components/password-reset";
 export default function Welcome() {
     return (
         <div className="h-screen flex justify-center items-center">
-            <div className="w-1/2">
+            <div className="w-1/2 flex flex-col items-center">
                 <h2 className="text-center text-2xl -mt-16 text-purple-400">
                     Let&apos;s
                 </h2>
                 <h1 className="text-center text-5xl mt-4 text-purple-500 font-bold">
                     CONNECT
                 </h1>
+                <p className="w-2/3 text-center text-xl mt-8 text-gray-500">
+                    A platform for aspiring software developers and career
+                    changers to share their stories and network with like-minded
+                    people{" "}
+                </p>
             </div>
             <HashRouter>
                 <div className="w-1/2 flex justify-center items-center">
