91221f69861f9d3ea393214c9c2702565e4bf213
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-05-27 11:03:37 +0200

    rewrites OtherProfile to be a function component

--- client/src/components/other-profile.js
+++ client/src/components/other-profile.js
@@ -1,59 +1,52 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import ProfilePicture from "../components/profile-picture";
 import ConnectButton from "../components/connect-button";
 import axios from "../axios";
 
-export default class OtherProfile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+export default function OtherProfile(props) {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [imgUrl, setImgUrl] = useState("");
+    const [bio, setBio] = useState("");
 
-    async componentDidMount() {
-        const { id } = this.props.match.params;
-        try {
-            const response = await axios.get(`/other-user/${id}`);
-            this.setState({
-                firstName: response.data["first_name"],
-                lastName: response.data["last_name"],
-                imgUrl: response.data["img_url"],
-                bio: response.data["bio"],
-            });
-        } catch (err) {
-            console.log(err);
-            this.props.history.replace("/");
-        }
-    }
+    useEffect(() => {
+        const { id } = props.match.params;
+        (async () => {
+            try {
+                const response = await axios.get(`/other-user/${id}`);
+                setFirstName(response.data["first_name"]);
+                setLastName(response.data["last_name"]);
+                setImgUrl(response.data["img_url"]);
+                setBio(response.data["bio"]);
+            } catch (err) {
+                console.log(err);
+                props.history.replace("/");
+            }
+        })();
+    }, []);
 
-    render() {
-        return (
-            <div className="mt-16 w-2/3 flex flex-col items-center bg-white shadow-lg rounded-lg">
-                <div className="flex flex-col justify-center -mt-12">
-                    <ProfilePicture
-                        imgUrl={this.state.imgUrl || "/user.png"}
-                        firstName={this.state.firstName}
-                        lastName={this.state.lastName}
-                        inProfile={true}
-                        defaultImg={this.state.imgUrl === null}
-                    />
-                </div>
-
-                <ConnectButton userId={this.props.match.params.id} />
-
-                <div className="flex flex-col items-center flex-grow py-12 md:w-3/5 lg:w-2/5">
-                    <h3 className="font-bold text-lg">About</h3>
-                    {this.state.bio ? (
-                        <p className="flex-grow text-center mt-4">
-                            {this.state.bio}
-                        </p>
-                    ) : (
-                        <p className="flex-grow text-center text-gray-500 mt-4">
-                            {this.state.firstName} hasn&apos;t added anything to
-                            bio yet
-                        </p>
-                    )}
-                </div>
+    return (
+        <div className="mt-16 w-2/3 flex flex-col items-center bg-white shadow-lg rounded-lg">
+            <div className="flex flex-col justify-center -mt-12">
+                <ProfilePicture
+                    imgUrl={imgUrl || "/user.png"}
+                    firstName={firstName}
+                    lastName={lastName}
+                    inProfile={true}
+                    defaultImg={imgUrl === null}
+                />
             </div>
-        );
-    }
+            <ConnectButton userId={props.match.params.id} />
+            <div className="flex flex-col items-center flex-grow py-12 md:w-3/5 lg:w-2/5">
+                <h3 className="font-bold text-lg">About</h3>
+                {bio ? (
+                    <p className="flex-grow text-center mt-4">{bio}</p>
+                ) : (
+                    <p className="flex-grow text-center text-gray-500 mt-4">
+                        {firstName} hasn&apos;t added anything to bio yet
+                    </p>
+                )}
+            </div>
+        </div>
+    );
 }
